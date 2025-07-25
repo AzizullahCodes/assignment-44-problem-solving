55fce0e90d6888e5dc871a7f911b4162dc2444cc
Azizullah <azizullahcodes@gmail.com>
2025-07-25 08:48:22 -0700

    u

---
+++ export.js
@@ -0,0 +1,14 @@
+// mathUtils.js
+
+// Named exports
+export function add(a, b) {
+  return a + b;
+}
+
+export function subtract(a, b) {
+  return a - b;
+}
+
+// Default export (optional)
+const pi = 3.14159;
+export default pi;
---
+++ import.js
@@ -0,0 +1,13 @@
+
+// main.js
+
+// Import named functions
+import { add, subtract } from './mathUtils.js';
+
+// Import default value
+import pi from './mathUtils.js';
+
+// Use them
+console.log("Add: ", add(10, 5));           // Output: Add: 15
+console.log("Subtract: ", subtract(10, 5)); // Output: Subtract: 5
+console.log("Value of pi: ", pi);           // Output: Value of pi: 3.14159
